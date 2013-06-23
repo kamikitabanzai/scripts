a467c750f1f0148f060abb48aecfcaaf3de1baf4
kamikitabanzai <kamikitabanzai@yahoo.co.jp>
2013-06-23 23:27:06 +0900

    sql

---
+++ countZipByPrefLow.sql
@@ -0,0 +1,9 @@
+select count(a.new_zip_id),a.pref
+from 
+(select new_zip_id,pref
+  from mst_zip
+  group by new_zip_id,pref
+  order by new_zip_id) a
+group by a.pref
+order by count(a.new_zip_id)
+;
