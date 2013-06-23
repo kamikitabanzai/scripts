12adddb3c2b3bf5199533d811d2808916f9487dd
kamikitabanzai <kamikitabanzai@yahoo.co.jp>
2013-06-23 10:41:52 +0900

    結果取得と出力を分けた

--- mycse.py
+++ mycse.py
@@ -23,6 +23,16 @@ class File():
     dsn = self.buff2dsn(buff)
     return dsn
 
+  def conFile2buff(self,conFile):
+    f=open(conFile,'r')
+    buff = {}
+    for line in f:
+      if len(line.split('=')) > 1:
+        paras = line.split('=')
+        buff[paras[0].strip()] = paras[1].strip()
+    f.close()
+    return buff 
+
   def buff2dsn(self,buff):
     dsn = {}
     dsn['host'] = buff['HOST']
@@ -30,22 +40,6 @@ class File():
     dsn['user'] = buff['DATABASE_USER']
     dsn['password'] = buff['DATABASE_PASS']
     return dsn
-
-  def conFile2buff(self,conFile):
-    f=open(conFile,'r')
-    buff = {}
-    for line in f:
-      if self.haveMeaning(line):
-        paras = line.split('=')
-        buff[paras[0].strip()] = paras[1].strip()
-    f.close()
-    return buff 
-
-  def haveMeaning(self,line):
-    if len(line.split('=')) <= 1:
-      return False
-    else:
-      return True  
 
   def returnSqls(self,sqlFile):
     longLine = ''
@@ -70,177 +64,80 @@ class File():
 
 class PgProcess():
 
-  def __init__(self):
+  def __init__(self,container):
     self._con = pgdb.connect(**DSN) 
     self._cur = self._con.cursor() 
+    self.explainService = ExplainService()
+    self.container = container
+    self.qID = 0
+    self.totalTime = 0
 
-  def run(self,queries):
-    end = 0
-    total = Total()
+  def run(self,sqls):
     try:
-      for q in queries:
-
-        explainService = ExplainService()
-        explain = explainService.getExplain(q)
-        
-        start = time.time()
-        self._cur.execute(q)
-        each = time.time() - start
-        end += each
-        
-        view = SqlViewer()
-        view.each = each
-        view.q = q
-        view.explain = explain
-        view.head = self._cur.description 
-        view.result = self._cur.fetchall()
-
-        view.view()
-
-        total.each.append(each)
-
-      total.end = end
-
-      self._cur.execute(INDEX_QUERY)
-      total.indexes = self._cur.fetchall()
-
-      total.view()
-
+      self.createSessionRunningSql(sqls)
     except :
       raise
     finally:
       self._con.close()
       self._cur.close()
-
     return
 
-class Total():
+  def createSessionRunningSql(self,sqls):
+    for sql in sqls:
+      self.runSql(sql,self.qID)
+      self.container.showOneResult(self.qID)
+      self.qID += 1
+    self.container.totalTime = self.totalTime
+    self._cur.execute(INDEX_QUERY)
+    self.container.indexes = self._cur.fetchall()
+    return
+  
+  def runSql(self,sql,qID):
+    eachTime = self.timeSql(sql)
+    self.totalTime += eachTime
+    self.container.eachTimes[qID] = eachTime
+    self.container.sqls[qID] = sql
+    self.container.plans[qID] = self.explainService.getExplain(sql)
+    self.container.heads[qID] = self._cur.description
+    self.container.results[qID] = self._cur.fetchall()
+    self.container.qIDs.append(qID)
+
+  def timeSql(self,sql):
+    startTime = time.time()
+    self._cur.execute(sql)
+    eachTime = time.time() - startTime
+    return eachTime
+
+class ResultContainer():
   def __init__(self):
-    self.each = []
-    self.end = 0
+    self.qIDs = []
+    self.heads = {}
+    self.sqls = {}
+    self.results = {}
+    self.plans = {}
+    self.eachTimes = {}
+    self.totalTime = 0
     self.indexes = []
   
-  def view(self):
-    self.indexShow(self.indexes)
-    self.totalShow()
-
-  def indexShow(self,indexes):
-    for i in indexes:
-      print i
-    print DIVIDER
-
-  def totalShow(self):
-    count = 0
-    for e in self.each:
-      count += 1
-      print '[%s]:%.2f' % (count,e)
-    print 'Total:%.2f sec' % self.end
+  def showOneResult(self,qID):
     print ''
-
-class ExplainService():
-
-  def __init__(self):
-    self._con = pgdb.connect(**DSN) 
-    self._cur = self._con.cursor() 
-    self.enableSqls = []
-
-  def cutHighCost(self,sqls):
-    for sql in sqls:
-      newSqls = []
-      plan = self.getExplain(sql)
-      if self.checkCost(plan):
-        newSqls.append(sql)
-      else:
-        cost = self.getCost(plan)
-        print 'this query dosn\'t'
-        print DIVIDER
-        print sql
-        print DIVIDER
-        print 'the cost of this query is over the limit cost=%.2f ' % (cost)
-
-    if len(newSqls) < 1:
-      raise NoEnableSql() 
-    
-    self.enableSqls = newSqls
-    return 
-
-  def getExplain(self,sql):
-    try:
-      exp_q = 'explain ' + sql
-      self._cur.execute(exp_q)
-      exp_r = self._cur.fetchall()
-    except:
-      raise
-    finally:
-      self._con.close()
-      self._cur.close()
-    return exp_r
-
-  def checkCost(self,plan):
-    cost = self.getCost(plan)
-    if cost < COST_LIMIT:
-      return True
-    else:
-      return False
-
-  def getCost(self,plan):
-    header = plan[0][0].split(' ')
-    cost = 0
-    for h in header:
-      if self.findCost(h): 
-        cost = self.returnCost(h) 
-        break
-    return cost
-  
-  def findCost(self,line):
-    if line.find('cost=') > 0:
-      return True
-    else:
-      return False
-
-  def returnCost(self,line):
-    strCost = line.split('..')[1]
-    return float(strCost)
-
-class NoEnableSql(BaseException):
-  def printException(self):
-    print DIVIDER
-    print "you have no enable sqls"
-
-class SqlViewer():
-
-  def __init__(self):
-    self.head = []
-    self.result = []
-    self.q = ''
-    self.each = 0
-    self.explain = []
-
-  def view(self):
-    self.queryShow(self.q) 
-    self.headShow(self.head) 
-    self.rowShow(self.result)
-    self.expShow(self.explain)
-    self.subShow()
-
-  def queryShow(self,query):
-    print ''
-    print query
-
-  def subShow(self):
-    print DIVIDER
-    print ' %d 行' % len(self.result)
-    print ' %.2f sec' % self.each
-    print QUERY_DIVIDER
-
-  def expShow(self,explain):
-    for exp in explain:
-      print exp
-
-  def headShow(self,heads):
-    headOut = self.makeHead(heads)
+    print self.sqls[qID]
+    headOut = self.makeHead(self.heads[qID])
     print headOut
-  
+    self.showResult(self.results[qID])
+    print ''
+    self.showLines(self.plans[qID])
+    print DIVIDER
+    print ' %d 行' % len(self.results[qID])
+    print ' %.2f sec' % self.eachTimes[qID]
+    print QUERY_DIVIDER
+ 
+  def showSummary(self):
+    self.showLines(self.indexes)
+    print DIVIDER
+    self.showTotal(self.eachTimes,self.totalTime)
+    print ''
+
   def makeHead(self,heads):
     headOut ='('
     for h in heads:
@@ -250,12 +147,11 @@ class SqlViewer():
     headOut +=')'
     return headOut
   
-  def rowShow(self,rows):
+  def showResult(self,result):
     limiter = 0
-    for row in rows:
+    for row in result:
       rowOut = self.makeRow(row)
       print rowOut
-
       limiter += 1
       if limiter >= ROW_LIMIT:
          break
@@ -265,14 +161,77 @@ class SqlViewer():
     for column in row:
       if not rowOut == ' [':
         rowOut += ','
-
       if isinstance(column,str):
         rowOut += column #.decode('utf-8')
       else:
         rowOut += str(column)
     rowOut += ']'
-
     return rowOut
+  
+  def showLines(self,lines):
+    for line in lines:
+      print line
+    return
+
+  def showTotal(self,eachTimes,totalTime):
+    count = 0
+    for eachTime in eachTimes.values():
+      count += 1
+      print '[%s]:%.2f' % (count,eachTime)
+    print 'Total:%.2f sec' % totalTime
+
+class ExplainService():
+
+  def __init__(self):
+    self._con = pgdb.connect(**DSN) 
+    self._cur = self._con.cursor() 
+    self.enableSqls = []
+ 
+  def cutHighCost(self,sqls):
+    try:
+      self.enableSqls = self.createSessionCutHighCost(sqls)
+    except:
+      raise
+    finally:
+      self._con.close()
+      self._cur.close()
+    if len(self.enableSqls) < 1:
+      raise NoEnableSql()
+    return
+
+  def createSessionCutHighCost(self,sqls):
+    enableSqls = []
+    for sql in sqls:
+      plan = self.getExplain(sql)
+      cost = self.getCost(plan)
+      if cost < COST_LIMIT:
+        enableSqls.append(sql)
+      else:
+        warnLimitOver(sql,cost)
+    return enableSqls 
+
+  def getExplain(self,sql):
+    exp_q = 'explain ' + sql
+    self._cur.execute(exp_q)
+    plan = self._cur.fetchall()
+    return plan
+
+  def getCost(self,plan):
+    header = plan[0][0].split(' ')
+    cost = 0
+    for h in header:
+      if h.find('cost=') > 0:
+        cost = float(h.split('..')[1]) 
+        break
+    return cost
+  
+  def warnLimitOver(self,sql,cost):
+    print 'this query dosn\'t'
+    print DIVIDER
+    print sql
+    print DIVIDER
+    print 'the cost of this query is over the limit cost=%.2f ' % (cost)
+    return
 
 class MyCseService:
   def __init__(self,sqlFile):
@@ -282,14 +241,23 @@ class MyCseService:
     DSN = file.returnDsn('con.con')
     self.sqls = file.returnSqls(sqlFile)
     self.explainService = ExplainService()
-    self.pgprocess = PgProcess()
+    self.container = ResultContainer()
+    # 実行結果をコンテナに格納する
+    self.pgprocess = PgProcess(self.container)
 
   def run(self):
     # ヘビーSQLの削除
     self.explainService.cutHighCost(self.sqls)
-
+    # SQLの実行と結果の出力
     enableSqls = self.explainService.enableSqls
     self.pgprocess.run(enableSqls)
+    # 実行結果まとめの出力
+    self.container.showSummary()
+
+class NoEnableSql(BaseException):
+  def printException(self):
+    print DIVIDER
+    print "you have no enable sqls"
 
 def main():
 
@@ -302,7 +270,6 @@ def main():
     
     service = MyCseService(argvs[1]) 
     service.run()
-    
   except Exception , e:
     print  e 
     import traceback
